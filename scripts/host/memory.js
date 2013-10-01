ec9445a67ba05cc9b4691a5111391fee66b580aa
BruceLaclair <bruce.laclair1@marist.edu>
2013-10-01 14:23:44 -0400

    First Try
    ACTUALLY RUNS PROGRAMS EXECUTING MEMORY IN CLOCK CYCLES IT"S LIKE YAY!

---
+++ scripts/host/memory.js
@@ -0,0 +1,60 @@
+/* ------------  
+   memory.js
+
+   Requires global.js.
+   
+   Creates, initializes, manages and updates the main memory of the OS.
+   ------------ */
+   
+function memory(){
+	this.memory = new Array();
+	
+	
+	this.init = function() {
+		for(i = 0; i < 768; i++)
+		{
+			this.memory[i] = "00";
+			document.getElementById(i).innerHTML=this.memory[i];
+		}
+	};
+	
+	this.checkLimit = function(locale) 
+	{
+		if (_PID === 1)
+		{
+			var limit = parseInt(locale,16);
+			if(limit >= 256)
+			{
+				krnTrapError("Hey! That's not yours to touch");
+			}
+			else
+			{
+				return limit;
+			}
+		}
+		else if (_PID === 2)
+		{
+			var limit = parseInt(locale,16);
+			if(limit >= 512)
+			{
+				krnTrapError("Hey! That's not yours to touch");
+			}
+			else
+			{
+				return limit;
+			}
+		}
+		else
+		{
+			var limit = parseInt(locale,16);
+			if(limit >= 768)
+			{
+				krnTrapError("Hey! That's not yours to touch");
+			}
+			else
+			{
+				return limit;
+			}
+		}
+	}
+}
